44fb428479fcdaf6d7a3a4650a28502a47910ded
Snok <chocowaco152@gmail.noreply.github.com>
2025-02-14 21:34:43 -1000

    Create tab switching logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import "./styles.css"
 import { createHome } from "./createHome.js"
+import { createMenu } from "./createMenu.js"
+import { createAbout } from "./createAbout.js"
+
+let content = document.querySelector("#content");
+let home = document.querySelector("#home");
+let menu = document.querySelector("#menu");
+let about = document.querySelector("#about");
 
 createHome();
+
+home.addEventListener("click", () => {
+    content.replaceChildren();
+    createHome()
+});
+
+menu.addEventListener("click", () => {
+    content.replaceChildren();
+    createMenu()
+});
+
+about.addEventListener("click", () => {
+    content.replaceChildren();
+    createAbout()
+});
