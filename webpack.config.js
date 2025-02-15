4db410fc8063afc8fd0982c32325d8b2ca592f60
Snok <chocowaco152@gmail.noreply.github.com>
2025-02-14 18:32:25 -1000

    Add dev server settings to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
